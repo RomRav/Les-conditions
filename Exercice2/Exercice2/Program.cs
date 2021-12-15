80efcfae5bbb35517f5cba9efa2b6830a9a2ed2d
RomRav <romain.ravault@gmail.com>
2021-12-15 12:11:36 +0100

    Ajout dans l'exercice 2 de la verification de la saisie

--- Exercice2/Exercice2/Program.cs
+++ Exercice2/Exercice2/Program.cs
@@ -6,17 +6,32 @@ namespace Exercice2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Qu'elle est votre âge?:");
-            string userStringAge = Console.ReadLine();
-            int userNumberAge = Convert.ToInt32(userStringAge);
-            if (userNumberAge >= 18)
+            bool finisher = true;
+            
+            int userNumberAge;
+            while (finisher)
             {
-                Console.WriteLine($"Vous avez {userNumberAge} ans, vous êtes donc majeur.e.");
+                Console.WriteLine("Qu'elle est votre âge?:");
+                string userStringAge = Console.ReadLine();
+                if (int.TryParse(userStringAge, out userNumberAge))
+                {
+                    if (userNumberAge >= 18)
+                    {
+                        Console.WriteLine($"Vous avez {userNumberAge} ans, vous êtes donc majeur.e.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Vous avez {userNumberAge} ans, vous êtes donc mineur.e.");
+                    }
+                    finisher = false;
+                }
+                else
+                {
+                    Console.WriteLine("La saisie n'est pas un nombre.");
+                }
             }
-            else
-            {
-                Console.WriteLine($"Vous avez {userNumberAge} ans, vous êtes donc mineur.e.");
-            }
+           
+            
         }
     }
 }
